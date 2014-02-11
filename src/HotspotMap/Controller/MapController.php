6e62f77d8d09690244bd851e59205da98077afff
florian <florian.rotagnon@gmail.com>
2014-02-11 15:50:29 +0100

    add controller struct

---
+++ src/HotspotMap/Controller/MapController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace HotspotMap\Controller;
+
+use Silex\Application;
+use Symfony\Component\HttpFoundation\Request;
+
+class MapController{
+
+    public function index(Request $request, Application $app){
+        return 'et ca marche bien';
+    }
+
+}
--- web/index.php
+++ web/index.php
@@ -6,12 +6,26 @@ $app = new Silex\Application();
 
 $app['debug'] = true;
 
-$app->get('/', function () {
-    return 'It works!';
-});
+$app->get('/', 'HotspotMap\\Controller\\MapController::index');
 
 $app->get('/hello/{name}', function ($name) use ($app) {
     return 'Hello '.$app->escape($name).'!';
+});
+
+
+$app->error(function(\Exception $e, $code) use($app) {
+    if($app['debug'])
+        return;
+
+    switch ($code)
+    {
+        case 404 :
+            $message='404 page to overload';
+            break;
+        default:
+            $message='Error not 404 to overload';
+    }
+    return $message;
 });
 
 $app->run();
