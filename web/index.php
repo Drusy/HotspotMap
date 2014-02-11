faa341ce42c86c93c02cc8bcb7c086e675f8baec
Kévin Renella <kevin.renella@gmail.com>
2014-02-11 15:58:04 +0100

    Add PHPUnit

--- web/index.php
+++ web/index.php
@@ -1,18 +1,22 @@
 <?php
 
 require_once __DIR__.'/../vendor/autoload.php';
+use Symfony\Component\HttpFoundation\Response;
 
 $app = new Silex\Application();
 
 $app['debug'] = true;
 
+// Index
 $app->get('/', 'HotspotMap\\Controller\\MapController::index');
 
+// Controllers
 $app->get('/hello/{name}', function ($name) use ($app) {
     return 'Hello '.$app->escape($name).'!';
-});
+})
+->value('name', 'unknown');
 
-
+// Error management
 $app->error(function(\Exception $e, $code) use($app) {
     if($app['debug'])
         return;
@@ -25,7 +29,7 @@ $app->error(function(\Exception $e, $code) use($app) {
         default:
             $message='Error not 404 to overload';
     }
-    return $message;
+    return new Response($message);
 });
 
 $app->run();
