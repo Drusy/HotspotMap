82e546c2afeebba411b25d8ae01a8c97229b0b12
Kévin Renella <kevin.renella@gmail.com>
2014-02-18 20:17:24 +0100

    Improve geolocalization method
    Use javacript to display the Map

--- src/HotspotMap/Controller/MapController.php
+++ src/HotspotMap/Controller/MapController.php
@@ -3,11 +3,43 @@
 namespace HotspotMap\Controller;
 
 use Silex\Application;
-use Symfony\Component\HttpFoundation\Request;
 
 class MapController extends HotspotMapController
 {
-    public function index( Application $app)
+    private $geocoder;
+    private $adapter;
+    private $buzz;
+    private $geolocalize;
+
+    private function retrieveClientInfo() {
+        return json_decode(file_get_contents("http://ipinfo.io/"), true);
+    }
+
+    public function __construct()
+    {
+        // Init GeoCoder
+        $this->buzz = new \Buzz\Browser(new \Buzz\Client\Curl());
+        $this->adapter = new \Geocoder\HttpAdapter\BuzzHttpAdapter($this->buzz);
+        $this->geocoder = new \Geocoder\Geocoder();
+
+        // Manage client Ip Address
+        $clientIp = $_SERVER['REMOTE_ADDR'];
+
+        if ($clientIp === '::1' || $clientIp === '127.0.0.1') {
+            $this->geolocalize = 'Clermont-Ferrand';
+            $this->geocoder->registerProviders(array(
+                new \Geocoder\Provider\GoogleMapsProvider($this->adapter, 'fr_FR', 'France', true)
+            ));
+
+        } else {
+            $this->geolocalize = $clientIp;
+            $this->geocoder->registerProviders(array(
+                new \Geocoder\Provider\FreeGeoIpProvider($this->adapter)
+            ));
+        }
+    }
+
+    public function index(Application $app)
     {
         $placeMapper = $app['PlaceMapper'];
         $userMapper = $app['UserMapper'];
@@ -20,8 +52,9 @@ class MapController extends HotspotMapController
             'places' => $places
         );
 
-        return $this->respond($app, 'data', $data, 'map/index');
+        $data['clientInfo'] = $this->retrieveClientInfo();
+        $data['geocoder'] = $this->geocoder->geocode($this->geolocalize);
 
-        return $message;
+        return $this->respond($app, 'data', $data, 'map/index');
     }
 }
